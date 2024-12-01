01c748c2bcc9153d82dadb172069d20d86293fd6
om13rajpal <omrajpal.exe@gmail.com>
2024-12-01 13:09:11 +0530

    designed the dashboard page

---
+++ lib/components/dashbutton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DashboardButton extends StatelessWidget{
+  final Color colour;
+  final String text;
+  const DashboardButton({super.key, required this.colour, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                          height: 48,
+                          width: MediaQuery.of(context).size.width * 0.27,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: colour,
+                              borderRadius: BorderRadius.circular(10)),
+                         child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                                child: Text(
+                              textAlign: TextAlign.center,
+                              text,
+                              style: const TextStyle(
+                                  fontSize: 11.5,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xff272727)),
+                            )),
+                          ),
+                        );
+  }
+
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,11 @@
 import 'package:drdo/components/background.dart';
+import 'package:drdo/components/dashbutton.dart';
+import 'package:drdo/components/job.dart';
 import 'package:drdo/components/mainheading.dart';
 import 'package:drdo/components/numbers.dart';
 import 'package:drdo/components/sectionheading.dart';
 import 'package:drdo/components/text.dart';
+import 'package:drdo/components/topexperts.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
@@ -17,31 +20,139 @@ class Homepage extends StatelessWidget {
           child: SizedBox(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width * 0.85,
-            child: const Column(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 50,
                 ),
-                Mainheading(text: "Dashboard"),
-                SizedBox(
+                const Mainheading(text: "Dashboard"),
+                const SizedBox(
                   height: 20,
                 ),
-                Row(
+                const Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Numbers(label: "Candidates", value: "11.26 K"),
                     Numbers(label: "Experts", value: "518"),
-                    Numbers(label: "Open Jobs", value: "1009"), 
+                    Numbers(label: "Open Jobs", value: "1009"),
                   ],
                 ),
-
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-
-                Sectionheading(text: "Top Experts")
+                const Sectionheading(text: "Top Experts"),
+                const SizedBox(
+                  height: 10,
+                ),
+                const TopExperts(
+                    name: "Om Rajpal", job: "Flutter Developer", score: "85.5"),
+                const SizedBox(
+                  height: 7,
+                ),
+                const TopExperts(
+                    name: "Jasvin", job: "Node.js Developer", score: "91.4"),
+                const SizedBox(
+                  height: 7,
+                ),
+                const TopExperts(
+                    name: "Abcd", job: "Docker Developer", score: "42.1"),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    print("view more");
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const TextData(
+                          text: "View more",
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff7E7E7E)),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Transform.translate(
+                          offset: const Offset(0, 1),
+                          child: const TextData(
+                              text: ">",
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromARGB(255, 100, 100, 100))),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Sectionheading(text: "Control Center"),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    DashboardButton(
+                        colour: Color(0xffDE8F6E), text: "View all\nexperts"),
+                    DashboardButton(
+                        colour: Color(0xff9CAFB7), text: "View all\njobs"),
+                    DashboardButton(
+                        colour: Color(0xff73A7A0),
+                        text: "View all\ncandidates"),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Sectionheading(text: "Recent Job Openings"),
+                const SizedBox(
+                  height: 7,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Job(jobTitle: "Node.js Developer"),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    const Job(jobTitle: "Flutter Developer"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print("view more");
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const TextData(
+                              text: "Search more",
+                              fontSize: 10,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff7E7E7E)),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Transform.translate(
+                              offset: const Offset(0, 1),
+                              child: const TextData(
+                                  text: ">",
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromARGB(255, 100, 100, 100))),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
